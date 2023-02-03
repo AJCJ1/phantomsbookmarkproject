c9075c9d4ac4da24f9bf6222e6976674ada248af
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2023-02-03 13:55:37 +0000

    Renders form to change state

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const Main = () => {
-
   // State to hold an array of all bookmarks
   const [bookmarks, setBookmarks] = useState([]);
 
@@ -9,20 +8,45 @@ const Main = () => {
   const [name, setName] = useState("");
   const [url, setUrl] = useState("");
 
-  return(
+  return (
+    <div>
+        {/* form to submit a new bookmark */}
+        <h2>Add a bookmark: </h2>
+        <div className="form-container">
+        <form
+          className="form-main"
+          onSubmit={async (e) => {
+            // Prevents default form refresh, completes input validation before submission
+            e.preventDefault();
+              setName("");
+              setUrl("");
+            }
+          }>
+          {/* input field to enter bookmark name */}
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Name"
+          maxLength="15"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {/* input field to enter bookmark URL */}
+        <input
+          className="input-field"
+          type="text"
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        {/* submit button to add/update a bookmark */}
+        <button className="submit-btn" type="submit">
+          Add bookmark
+        </button>
+        </form>
+        </div>
+      </div>
+  );
+};
 
-
-
-
-
-
-
-    <div>Main</div>
-
-
-
-  )
-
-}
-
-export default Main
+export default Main;
