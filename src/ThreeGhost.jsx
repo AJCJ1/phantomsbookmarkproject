4defeb737a61aea241b7bf33afd2bcc0f0fb69c5
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2023-02-03 21:43:54 +0000

    resizes canvas on viewport resize

--- src/ThreeGhost.jsx
+++ src/ThreeGhost.jsx
@@ -3,6 +3,14 @@ import * as THREE from "three";
 
 const ThreeGhost = () => {
 
+  // add an event listener to the window to handle resizing
+  window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // update the camera's projection matrix
+    camera.updateProjectionMatrix();
+  });
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
