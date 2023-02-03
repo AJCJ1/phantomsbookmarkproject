b740fd799c64abedeee2f1b790a041950b160412
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2023-02-03 21:47:12 +0000

    Creates a low-light ghoulish scene with lighting

--- src/ThreeGhost.jsx
+++ src/ThreeGhost.jsx
@@ -15,16 +15,27 @@ const ThreeGhost = () => {
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+// antialias allows edge smoothing, makes graphics look cleaner
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
   const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  // create a material for the ghost, that computes light at every pixel
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x00fdc7,
+    transparent: false,
+    opacity: 8,
+  });
   const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  scene.add(cube);
 
   camera.position.z = 5;
+
+  // new light to shine on ghost, white, 3 intensity and 100 distance
+  var light = new THREE.PointLight(0xffffff, 3, 100);
+  light.position.set(50, 50, 50);
+  scene.add(light);
 
 
   // function to animate the scene using requestAnimationFrame API
